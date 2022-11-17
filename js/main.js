d142eb0749d8024bb2385f74f79692052ff52f2c
smboolean76 <smboolean76@madrigali.dev>
2022-11-17 10:35:07 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        emails: [],
+        error: false,
+    }
+  },
+  created() {
+    for( let i = 1; i <= 10; i++ ) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            this.emails.push(response.data.response);
+        })
+        .catch(() => {
+            this.error = true
+        });
+    }
+  }
+}).mount('#app');
